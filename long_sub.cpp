2a0a76c8145ca4acc94c2d9a4cc301aa66308b08
uvwone <120762631+uvwone@users.noreply.github.com>
2023-06-03 13:04:33 +0900

    Update long_sub.cpp

--- long_sub.cpp
+++ long_sub.cpp
@@ -5,8 +5,14 @@ using namespace std;
 
 int main()
 {
-    string a, b;
+    string a, b, sign;
     cin >> a >> b;
+
+    if(a.size() < b.size() || (a.size() == b.size() && a < b))
+    {
+        sign = "-";
+        swap(a,b);
+    }
 
     reverse(a.begin(),a.end());
     reverse(b.begin(),b.end());
@@ -15,10 +21,21 @@ int main()
     for(auto k : a) x.push_back(k - '0');
     for(auto k : b) y.push_back(k - '0');
 
-    for(int i=0; i<y.size(); i++) x[i] -= y[i];
+    for(int i=0; i<y.size(); i++)  x[i] -= y[i];
+
+    for(int i=0; i<x.size(); i++)
+    {
+        if(x[i] < 0)
+        {
+            x[i] += 10;
+            x[i+1] -= 1;
+        }
+    }
 
     while(x.size() > 1 && x.back() == 0) x.pop_back();
     reverse(x.begin(),x.end());
+
+    cout << sign;
     for(auto k : x)   cout << k;
     return 0;
 }
