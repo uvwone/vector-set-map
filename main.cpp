085fd106c26ffaf02f31b13521080cdb8335d460
uvwone <120762631+uvwone@users.noreply.github.com>
2023-06-17 09:45:00 +0900

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,31 +1,18 @@
-#include <iostream> //https://notepad96.tistory.com/25
+#include <iostream>
 #include <vector>
 #include <set>
-#include <algorithm>
 
 using namespace std;
-
+//°³¼ö Å½»ö
 int main()
 {
-    set<int> set;  //a : less<int>
-    //set<int, greater<int>> set; //
+    set<int> set;
 
     set.insert(30);
     set.insert(40);
     set.insert(10);
     set.insert(50);
-    set.insert(50);  // x
 
-    for(int i : set) cout << i<< " ";
-    cout <<"\n";
-    for(auto it = set.begin(); it!= set.end(); it++) cout << *it << " ";
-    cout << "\n";
-
-    //search
-    //1.
-    auto it = set.insert(50); //pair<set<int>::iterator, bool>
-    if(it.second) cout << *it.first << " success\n";
-    else cout << *it.first << " fail\n";
-
+    cout << "element 60 count: " << set.count(60) << "\n";
     return 0;
 }
